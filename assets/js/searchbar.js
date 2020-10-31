7bb1766764efdab5997da6f7ce6024179de73b19
Somfic <somfic@gmail.com>
2020-10-31 20:49:04 +0100

    maybe fixed it

--- assets/js/searchbar.js
+++ assets/js/searchbar.js
@@ -1,82 +1,74 @@
 $.extend($.expr[':'], {
-    'containsi': function(elem, i, match, array) {
-      return (elem.textContent || elem.innerText || '').toLowerCase()
-          .indexOf((match[3] || "").toLowerCase()) >= 0;
+    'containsi': function (elem, i, match, array) {
+        return (elem.textContent || elem.innerText || '').toLowerCase()
+            .indexOf((match[3] || "").toLowerCase()) >= 0;
     }
 });
 
+let searchbox = $('#searchbox');
 
-if(window.location.pathname.startsWith("/eliteapi/voiceattack/")) {
-    prepareSidebar("/eliteapi/voiceattack/static/sidebar.html");
-}
+searchbox.on('input', () => {
+    let searchterm = searchbox.val();
 
-function prepareSidebar(url) {
-        let sidebar = $('.sidebar');
-
-        sidebar.load(url, () => {
-
-            let searchbox = $('#searchbox');
-
-            searchbox.on('input', () => {
-                let searchterm = searchbox.val();
-                
-                if (searchterm == '') { 
-                    sidebar.attr('isFiltered', 'false');
-                    let matches = sidebar.find('label, a');
-                    matches.each((index, e) => {
-                        let item = $(e);
-                        check(item, searchterm);
-                    });
-                     $('.searchresult').removeClass('searchresult');
-                } else {
-                    sidebar.attr('isFiltered', 'true');
-                    let matches = sidebar.find('label:containsi("'+searchterm+'"), a:containsi("'+searchterm+'")');
-                    let invalidMatches = sidebar.find(':not(label:containsi("'+searchterm+'")), :not(a:containsi("'+searchterm+'"))');
-    
-                    $('.searchresult').removeClass('searchresult');
-
-                    invalidMatches.each((index, e) => {
-                        let item = $(e);
-                        uncheck(item);
-                    });
-    
-                    matches.each((index, e) => {
-                        let item = $(e);
-                        check(item, searchterm);
-                        mark(item);
-                    });
-                }
-            })
+    if (searchterm == '') {
+        sidebar.attr('isFiltered', 'false');
+        let matches = sidebar.find('label, a');
+        matches.each((index, e) => {
+            let item = $(e);
+            check(item, searchterm);
         });
-}
+        $('.searchresult').removeClass('searchresult');
+    } else {
+        sidebar.attr('isFiltered', 'true');
+        let matches = sidebar.find('label:containsi("' + searchterm + '"), a:containsi("' + searchterm + '")');
+        let invalidMatches = sidebar.find(':not(label:containsi("' + searchterm + '")), :not(a:containsi("' + searchterm + '"))');
 
-function check(i, searchterm) {
-    let item = $(i);
+        $('.searchresult').removeClass('searchresult');
 
-    if(item[0] == undefined) {return;}
+        invalidMatches.each((index, e) => {
+            let item = $(e);
+            uncheck(item);
+        });
 
-    let checkbox = item.find('input[type="checkbox"]');
-    checkbox.prop('checked', true);
-    
-    let parent = item.parent();
+        matches.each((index, e) => {
+            let item = $(e);
+            check(item, searchterm);
+            mark(item);
+        });
+    }
 
-    check(parent.closest('.item'));
-}
+    function check(i, searchterm) {
+        let item = $(i);
 
-function uncheck(i) {
-    let item = $(i);
+        if (item[0] == undefined) {
+            return;
+        }
 
-    if(item[0] == undefined) {return;}
+        let checkbox = item.find('input[type="checkbox"]');
+        checkbox.prop('checked', true);
 
-    let checkbox = item.find('input[type="checkbox"]');
-    checkbox.prop('checked', false);
+        let parent = item.parent();
 
-    let parent = item.parent();
+        check(parent.closest('.item'));
+    }
 
-    uncheck(parent.closest('.item'));
-}
+    function uncheck(i) {
+        let item = $(i);
 
-function mark(i) {
-    let item = $(i);
-    item.addClass('searchresult');
-}
+        if (item[0] == undefined) {
+            return;
+        }
+
+        let checkbox = item.find('input[type="checkbox"]');
+        checkbox.prop('checked', false);
+
+        let parent = item.parent();
+
+        uncheck(parent.closest('.item'));
+    }
+
+    function mark(i) {
+        let item = $(i);
+        item.addClass('searchresult');
+    }
+});
